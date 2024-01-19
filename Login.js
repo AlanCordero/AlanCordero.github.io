c69224be66cef45699cc9b8dd756fc32cfeb382f
AlanCordero <156855819+AlanCordero@users.noreply.github.com>
2024-01-18 21:12:11 -0600

    Update Login.js

--- Login.js
+++ Login.js
@@ -2,7 +2,7 @@ document.addEventListaner('DOMContentLoaded',funtion(){
   console.log("Hello,Word");
 
   var inputEmail = document.getElementById('email');
-var inputEmail = document.getElementById('password');
+var inputPassword = document.getElementById('password');
 console.log(inputEmail);
 console.log(inputPassword);
 
