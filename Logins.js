089f5b44c6c17e699aa53d37b30a1f3bc64bb319
AlanCordero <156855819+AlanCordero@users.noreply.github.com>
2024-01-25 00:53:15 -0600

    Create Logins.js

---
+++ Logins.js
@@ -0,0 +1,32 @@
+const nombre= document.getElementById("name")
+const email= document.getElementById("email")
+const password= document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnigs")
+
+form.addEventListener('submit', e=>{
+    e.preventDefault()
+    let warnigs=""
+    let regexEmail= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    let entrar= false
+    parrafo.innerHTML=""
+    if(nombre.value.lenght<5){
+    warnigs += 'El nombre que ingresaste es demasiado corto'    
+        entrar=true
+}
+        if(!regexEmail.test(email.value)){
+            warnigs += 'El email que ingresaste no es valido'    
+            entrar=true
+
+        }
+    if(password.value.lenght<8){
+        warnigs += 'La contraseña que ingresaste es demasiado corto'    
+            entrar=true
+    }
+    if(entrar){
+        parrafo.innerHTML=warnigs
+    }else{
+        parrafo.innerHTML="Enviado"
+
+    }
+})
